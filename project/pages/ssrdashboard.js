29fd06fc16ec804da5161dc07de6c1df8d06e35c
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-11-18 16:44:51 +0330

    use serverside authentication in sserDashboard

--- project/pages/ssrdashboard.js
+++ project/pages/ssrdashboard.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import { getSession } from "next-auth/react";
+import React from "react";
 
-const SSRDashboard = () => {
-    return (
-        <div>
-            <h1>SSR Dashboard</h1>
-        </div>
-    );
+const SSRDashboard = ({ session }) => {
+  return (
+    <div>
+      <h1>SSR Dashboard</h1>
+    </div>
+  );
 };
 
 export default SSRDashboard;
+
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
+}
