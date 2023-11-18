63f98af91a3060acdaff695c65872c821fd37d49
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-11-18 16:57:34 +0330

    handle buttons according authentications

--- project/pages/index.js
+++ project/pages/index.js
@@ -1,8 +1,10 @@
-import { signIn,signOut } from "next-auth/react";
-import Link from 'next/link';
+import { signIn, signOut } from "next-auth/react";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const { status } = useSession();
   const signinHandler = () => {
     signIn("github");
   };
@@ -13,10 +15,20 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Next-Auth|Github</h1>
-      <button onClick={signinHandler}>Signin</button>
-      <button onClick={signoutHandler}>Signout</button>
-      <button><Link href="/dashboard">Dashboard</Link></button>
-      <button><Link href="/ssrdashboard">SSR Dashboard</Link></button>
+      {status === "unauthenticated" ? (
+        <button onClick={signinHandler}>Signin</button>
+      ) : null}
+      {status === "authenticated" ? (
+        <>
+          <button onClick={signoutHandler}>Signout</button>
+          <button>
+            <Link href="/dashboard">Dashboard</Link>
+          </button>
+          <button>
+            <Link href="/ssrdashboard">SSR Dashboard</Link>
+          </button>
+        </>
+      ) : null}
     </div>
   );
 }
